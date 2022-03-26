12eb81cc5cc51485f78d078b8a8cd40ae22733c4
KegFmn <1165624857@qq.com>
2022-03-26 12:23:27 +0800

    增加多个生产者9002

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/likc/springcloud/PaymentMain9002.java
@@ -0,0 +1,20 @@
+package com.likc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author likc
+ * @date 2022/3/26
+ * @description
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+
+}
